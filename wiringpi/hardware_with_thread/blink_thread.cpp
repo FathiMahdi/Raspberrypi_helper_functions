19b44d12b593f4cd7ebe51b902ac86f5593a8c42
FathiMahdi <electronics_forever@outlook.com>
2021-09-22 15:33:40 +0800

    Changes to be committed: 	deleted:    Makefile 	deleted:    blink 	deleted:    blink.c 	deleted:    blink.cpp 	modified:   hardware_with_thread/Makefile 	modified:   hardware_with_thread/blink_thread.cpp

--- wiringpi/hardware_with_thread/blink_thread.cpp
+++ wiringpi/hardware_with_thread/blink_thread.cpp
@@ -4,16 +4,19 @@
 #include <wiringPi.h>
 using namespace std;
 
+const int red = 21;
+const int green = 17;
+
 void *thread_function_1(void *value)
 {
 	int *x = (int *)value;	
-	for(;;;)
+	for(;;)
 	{
 		cout << "Thread 1\n";
-		digitalWrite(*x,HIGH);
-		delay(1000);
-		digitalWrite(*x,LOW);
-		delay(1000);
+		digitalWrite(red,HIGH);
+		delay(50);
+		digitalWrite(red,LOW);
+		delay(50);
 	}
 	return (void *)0; // return a zero pointer
 }
@@ -21,27 +24,28 @@ void *thread_function_1(void *value)
 void *thread_function_2(void *value)
 {
 	int *x = (int *)value;	
-	for(;;;)
+	for(;;)
 	{
 		cout <<"Thread 2\n";
-		digitalWrite(*x,HIGH)
-		delay(1000);
-		digitalWrite(,LOW);
-		delay(1000);
+		digitalWrite(green,HIGH);
+		delay(200);
+		digitalWrite(green,LOW);
+		delay(200);
 	}
 	return (void *)0; // return a zero pointer
 }
 
 int main()
 {
-	int x = 0;
-	int green = 17;
-	int red = 21;
+	void *x;
 	void *ret;
+	wiringPiSetupGpio();
+	pinMode(red,OUTPUT);
+	pinMode(green,OUTPUT);
 	pthread_t Mythread1; // create thread object
 	pthread_t Mythread2; // create thread object
-	pthread_create(&Mythread1,NULL, &thread_function_1,&red); // create thread No.1
-	pthread_create(&Mythread2,NULL, &thread_function_2,&green); // create thread No.2
+	pthread_create(&Mythread1,NULL, &thread_function_1,&x); // create thread No.1
+	pthread_create(&Mythread2,NULL, &thread_function_2,&x); // create thread No.2
 	pthread_join(Mythread1,&ret);
 	pthread_join(Mythread2,&ret);
 
